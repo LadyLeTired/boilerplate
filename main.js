e0da2f37fbae95bf0aaa6ba7805111387c8684dd
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2018-12-20 21:23:22 -0500

    fixed some dependency issues

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-'use strict'
-const { db } = require('./server/db')
-const app = require('./server')
-const PORT = process.env.PORT || 8000
+"use strict";
+const { db } = require("./server/db");
+const app = require("./server");
+const PORT = process.env.PORT || 5555;
 // const Dummy = require('./server/db/models/dummyModel')
 
 const init = async () => {
   // await Dummy.sync({ force: false })
-  await db.sync({ force: false })
-  console.log('database did the thing')
-  app.listen(PORT, () => console.log(`app is currently doing the thing on Port ${PORT}`))
-}
+  await db.sync({ force: false });
+  console.log("database did the thing");
+  app.listen(PORT, () =>
+    console.log(`app is currently doing the thing on Port ${PORT}`)
+  );
+};
 
 init();
